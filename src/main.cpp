008c2424bd37b9acedb91c24b8a9c510e06eff6f
person1 <person1@smth.ru>
2025-09-19 22:25:38 +0300

    Add postgresql lib as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <pqxx/pqxx>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +11,8 @@ int main(int argc, char *argv[])
         std::cout << "Использование: " << argv[0] << " <действие> число1 [число2] ..." << std::endl;
         return 1;
     }
+
+    std::string input = argv[1], output;
 
     std::unique_ptr<IAction> action = createAction(argv[1]);
 
@@ -30,8 +33,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    input += argv[2];
+
     for (int i = 3; i < argc; ++i)
     {
+        input += std::string(" ") + argv[i];
         try
         {
             double num = std::stod(argv[i]);
@@ -43,6 +49,31 @@ int main(int argc, char *argv[])
         }
     }
     std::cout << "Результат: " << result << std::endl;
+    output = std::to_string(result);
+
+    try {
+        pqxx::connection C("dbname=postgres user=postgres password=2811 host=localhost port=5432");
+        if (C.is_open()) {
+            std::cout << "Successfully connected to the database: " << C.dbname() << std::endl;
+        } else {
+            std::cout << "Failed to connect to the database." << std::endl;
+            return 1;
+        }
+
+        pqxx::work W(C);
+
+        std::string sql = "INSERT INTO public.history (input, output) VALUES ($1, $2)";
+
+        W.exec(pqxx::zview{sql}, pqxx::params{input, output});
+
+        W.commit();
+
+        std::cout << "Data successfully inserted into the database." << std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
